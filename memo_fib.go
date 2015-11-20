1c2d0f58b06cc7458c7957f8cc03499ab23da22b
Max <max@carrotcreative.com>
2015-11-20 18:30:20 -0500

    add go memo_fib

---
+++ memo_fib.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+import "time"
+
+var memo map[int]int
+
+func Fib(n int) int {
+  if x, ok := memo[n]; ok {
+    return x
+  }
+  if n < 2 {
+    memo[n] = n
+    return memo[n]
+  }
+  memo[n] = Fib(n-1) + Fib(n-2)
+  return memo[n]
+}
+
+func main() {
+  timestart := time.Now()
+  memo = make(map[int]int)
+  memo[0] = 0
+  fmt.Printf("%v\n",Fib(30))
+  timeend := time.Now()
+  fmt.Print(timeend.Sub(timestart))
+}
